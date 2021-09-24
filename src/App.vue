4ac6a1a40bcf395ab9d60950c2e6b0e5b1b82b65
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-24 11:46:58 +0200

    API import

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,13 @@
 export default {
   name: 'App',
   components: {
-    
+    data() {
+      return {
+        APIurl: 'https://api.themoviedb.org/3/search/movie?',
+        APIkey: 'api_key=ddb4c15369c26b2f75ed68bdbbe010b8&language=it-IT',
+        APIquery: '',
+      }
+    }
   }
 }
 </script>
