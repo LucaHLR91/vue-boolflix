e848143ff068f802d178b6cf62f8f144f84a6b36
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-24 12:33:28 +0200

    Import logo, bootstrap e header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div id="app">
-    
+    <Header />
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
+// import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    data() {
+    Header
+  },
+  data() {
       return {
         APIurl: 'https://api.themoviedb.org/3/search/movie?',
         APIkey: 'api_key=ddb4c15369c26b2f75ed68bdbbe010b8&language=it-IT',
         APIquery: '',
       }
     }
-  }
 }
 </script>
 
 <style lang="scss">
+@import './style/General';
 #app {
   
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="d-flex flex-row justify-content-between">
+        <div id="logo">
+            <img src="../assets/img/Netflix-Logo.png" alt="Netflix Logo">
+        </div>
+        <div class="search">
+            <form action="">
+                <input type="text">
+            </form>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss">
+@import '../style/General';
+@import 'bootstrap';
+
+    header {
+        background-color: black;
+        height: 70px;
+
+        #logo {
+            height: 100%;
+        }
+
+        #logo img {
+            width: 70px;
+            
+        }
+    }
+</style>
