ad4d7c265862f0daadea191d0d5e97276ea6eb7a
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-27 11:06:38 +0200

    Data array flags

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -10,7 +10,19 @@
 <script>
 export default {
     name: 'Content',
-    props: ['info']
+    props: ['info'],
+    data() {
+        return {
+            flags: [
+                '../assets/img/flags/ItalianFlag.png',
+                '../assets/img/flags/AmericanFlag.png',
+                '../assets/img/flags/DeutchFlag.jpg',
+                '../assets/img/flags/FrenchFlag.png',
+                '../assets/img/flags/JapaneseFlag.png',
+                '../assets/img/flags/SpanishFlag.png',
+            ]
+        }
+    }
 }
 </script>
 
