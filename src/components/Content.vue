197491311ac4105bff793f14d01fa1ffa9a689f4
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-27 11:38:43 +0200

    flags

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -11,18 +11,7 @@
 export default {
     name: 'Content',
     props: ['info'],
-    data() {
-        return {
-            flags: [
-                '../assets/img/flags/ItalianFlag.png',
-                '../assets/img/flags/AmericanFlag.png',
-                '../assets/img/flags/DeutchFlag.jpg',
-                '../assets/img/flags/FrenchFlag.png',
-                '../assets/img/flags/JapaneseFlag.png',
-                '../assets/img/flags/SpanishFlag.png',
-            ]
-        }
-    }
+    
 }
 </script>
 
